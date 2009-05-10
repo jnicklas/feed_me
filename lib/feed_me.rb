e2969dd33c0adfc31775987a9260ba0ddf53581e
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-05-10 21:42:29 +0200

    Cleanup

--- lib/feed_me.rb
+++ lib/feed_me.rb
@@ -1,14 +1,4 @@
-<<<<<<< HEAD:lib/feed_me.rb
-# make sure we're running inside Merb
-if defined?(Merb::Plugins)
-  dependency 'hpricot'
-else
-  require 'rubygems'
-  require 'hpricot'
-end
-=======
 require 'hpricot'
->>>>>>> No longer failing Time parsing specs:lib/feed_me.rb
 require 'time'
 
 module FeedMe
